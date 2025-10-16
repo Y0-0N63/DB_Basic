6cf9cd77166cae67e64e103aa1be6e7092aef252
Y0-0N63 <boyun1625@naver.com>
2025-10-16 16:33:02 +0900

    DB 사용자 계정 생성 및 데이터 저장

--- 사용자계정생성script.sql
+++ 사용자계정생성script.sql
@@ -6,8 +6,7 @@ ALTER SESSION SET "_ORACLE_SCRIPT" = TRUE;
 
 -- 새로운 사용자 계정 생성 (USERNAME/Password)
 CREATE USER kh_cby IDENTIFIED BY kh1234;
--- SQL Error [65096] [99999]: ORA-65096: 공통 사용자 또는 롤 이름이 부적합합니다.
--- 11G 이전 문법 사용 허용으로 해결할 수 있음
+-- SQL Error [65096] [99999]: ORA-65096: 공통 사용자 또는 롤 이름이 부적합합니다. > 11G 이전 문법 사용해주기
 -- SQL Error [1920] [42000]: ORA-01920: 사용자명 'KH_CBY'(이)가 다른 사용자나 롤 이름과 상충됩니다
 
 -- 사용자 계정에 권한 부여
